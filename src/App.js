fc2f03e659c24e5a0fe86e0f04a50c4c1cffef04
jstukenb <jstukenb@lion.lmu.edu>
2020-01-30 17:15:47 -0800

    both add and delete functioning

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import TodoList from './TodoList'
+import TodoEntryForm from './TodoEntryForm'
 
 export default function App() {
   const [todos, setTodos] = useState([
-    {id: 1, content: 'Meet Masao for Udon'},
-    {id: 2, content: 'Fix up these horrible React notes'},
+    {id: 1, content: 'Finish todo app'},
+    {id: 2, content: 'eat dinner'},
   ]);
   function deleteTodo(id) {
     setTodos(todos => todos.filter(todo => todo.id !== id));
+  }
+  function addTodo(todo) {
+    setTodos(todos => [...todos, todo]);
   }
   return (
     <div className="App">
       <h1>Things To Do</h1>
       <TodoList todos={todos} deleter={deleteTodo} />
+      <TodoEntryForm adder={addTodo} />
     </div>
   );
 }
